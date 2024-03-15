9d062fb6ad270c97525ff27dcb8df2d49fbb2e2b
Rob B <rob@rjrobbie.dev>
2024-03-15 21:09:20 +0000

    added event listener to prevent deafult on modal form submit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
+const loginForm = document.getElementById("login-form")
 
 setTimeout(function () {
   modal.style.display = "block";
@@ -10,3 +11,7 @@ modalCloseBtn.addEventListener("click", function () {
   console.log("button is clicked");
   modal.style.display = "none";
 });
+
+loginForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+})

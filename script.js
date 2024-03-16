4c24baebebc8f60bbe0a54da36958ce8b7a5573b
Rob B <rob@rjrobbie.dev>
2024-03-16 20:12:23 +0000

    event listener now updates innerHTML on submit and again after 1500 ms

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const modal = document.getElementById("modal");
-const modalCloseBtn = document.getElementById("modal-close-btn");
-const loginForm = document.getElementById("login-form")
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
-setTimeout(function () {
-  modal.style.display = "block";
-  console.log("Modal Opened!");
-}, 1500);
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
 
-modalCloseBtn.addEventListener("click", function () {
-  console.log("button is clicked");
-  modal.style.display = "none";
-});
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
 
-loginForm.addEventListener('submit', function (e) {
-  e.preventDefault()
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>`
+    setTimeout(function(){
+        const uploadText = document.getElementById('upload-text')
+        uploadText.innerHTML = `Making the sale...`
+    }, 1500) 
+  
+   
+
 })

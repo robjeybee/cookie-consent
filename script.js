eb8004044003ae0363d0f5121af4518de10feed8
Rob B <rob@rjrobbie.dev>
2024-03-17 21:31:17 +0000

    Took data from a formData object, extract that data, and then use it in the application

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-const modal = document.getElementById('modal')
-const modalCloseBtn = document.getElementById('modal-close-btn')
-const consentForm = document.getElementById('consent-form')
-const modalText = document.getElementById('modal-text')
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
+const declineBtn = document.getElementById("decline-btn")
+const modalBtns = document.getElementById("modal-choice-btns")
 
-setTimeout(function(){
-    modal.style.display = 'inline'
-}, 1500)
+setTimeout(function () {
+  modal.style.display = "inline";
+}, 1500);
 
-modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none'
-})
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
 
-consentForm.addEventListener('submit', function(e){
-    e.preventDefault()
-    modalText.innerHTML = `
+consentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(consentForm);
+
+  const name = formData.get("fullName");
+
+  modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
         <p id="upload-text">Uploading your data to the dark web...</p>
-    </div>`
-    setTimeout(function(){
-        const uploadText = document.getElementById('upload-text')
-        uploadText.innerHTML = `Making the sale...`
-    }, 1500) 
-  
-   
+    </div>`;
+  setTimeout(function () {
+    document.getElementById("upload-text").innerText = `Making the sale...`;
+  }, 1500);
 
+  setTimeout(function () {
+    document.getElementById("modal-inner").innerHTML =
+      `<h2>Thank you <span class="modal-display-name">${name}</span>, you sucker!</h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    `;
+    modalCloseBtn.disabled = false;
+  }, 3000);
+});
+
+declineBtn.addEventListener("mouseenter", function () {
+    modalBtns.classList.toggle('reverse')
 })
